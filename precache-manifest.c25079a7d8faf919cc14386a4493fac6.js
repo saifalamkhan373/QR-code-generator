2bc9d80c232b766862ab92fa20df994ac542c587
Saif Alam Khan <54716802+saifalamkhan373@users.noreply.github.com>
2023-08-17 02:13:16 +0530

    Updates

---
+++ precache-manifest.c25079a7d8faf919cc14386a4493fac6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b272576d6349d94e4ca1f83b2eb98f47",
+    "url": "/QR-code-generator/index.html"
+  },
+  {
+    "revision": "65fa61c17a90a4d16e01",
+    "url": "/QR-code-generator/static/css/main.6ba31f66.chunk.css"
+  },
+  {
+    "revision": "d0349d4b3130741fcdd8",
+    "url": "/QR-code-generator/static/js/2.4cc04671.chunk.js"
+  },
+  {
+    "revision": "65fa61c17a90a4d16e01",
+    "url": "/QR-code-generator/static/js/main.bc05fc9a.chunk.js"
+  },
+  {
+    "revision": "5ae54b4c400bf04960c3",
+    "url": "/QR-code-generator/static/js/runtime~main.74c08cb4.js"
+  }
+]);
