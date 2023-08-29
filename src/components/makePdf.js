5e1f920618ed4441c4b4646ef209084f1bc7a1ac
Saif Alam Khan <54716802+saifalamkhan373@users.noreply.github.com>
2023-08-29 14:24:01 +0530

    fixed issue regarding pdf

--- src/components/makePdf.js
+++ src/components/makePdf.js
@@ -3,14 +3,20 @@ import html2canvas from "html2canvas";
 
 window.html2canvas = html2canvas;
 
-const makePDF = () => {
-  html2canvas(document.body).then(canvas => {
-    let pdf = new jsPDF("p", "mm", "a4");
-    pdf.addHTML(document.getElementById("makePdf"), () => {
-      pdf.addImage(canvas.toDataURL("image/png"), "PNG", 1, 0, 211, 298);
+function makePDF (){
+  html2canvas(document.querySelector("#invoiceCapture")).then((canvas) => {
+    const imgData = canvas.toDataURL('image/png', 1.0);
+    const pdf = new jsPDF({
+      orientation: 'portrait',
+      unit: 'pt',
+      format: [612, 792]
     });
-    let ps_filename = "QR_CODE";
-    pdf.save(ps_filename + ".pdf");
+    pdf.internal.scaleFactor = 1;
+    const imgProps= pdf.getImageProperties(imgData);
+    const pdfWidth = pdf.internal.pageSize.getWidth();
+    const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+    pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+    pdf.save('invoice-001.pdf');
   });
-};
+}
 export default makePDF;
